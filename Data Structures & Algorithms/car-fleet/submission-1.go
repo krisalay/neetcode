3eee163a8684d63e734edca41adc3f4abb136dfe
Kumar Kisalay <krisalay@outlook.com>
2026-06-27 21:18:15 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.go
@@ -0,0 +1,23 @@
+type Car struct {
+    Position int
+    Speed int
+}
+
+func carFleet(target int, position []int, speed []int) int {
+    cars := []Car{}
+    for i, pos := range position {
+        cars = append(cars, Car{Position: pos, Speed: speed[i]})
+    }
+    sort.Slice(cars, func(i, j int) bool {
+        return cars[i].Position > cars[j].Position
+    })
+
+    stack := []float64{}
+    for _, car := range cars {
+        timeTaken := float64(target-car.Position)/float64(car.Speed)
+        if len(stack) == 0 || timeTaken > stack[len(stack)-1] {
+            stack = append(stack, timeTaken)
+        }
+    }
+    return len(stack)
+}
