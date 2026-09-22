1001f1abce748b25c2bf4c421b1a41f3e0b51bff
Kumar Kisalay <krisalay@outlook.com>
2026-09-22 17:12:44 +0530

    Add: find-target-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-4.go
@@ -0,0 +1,23 @@
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left + right)/2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[left] <= nums[mid] { //left is sorted
+			if target >= nums[left] && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else { // right is sorted
+			if target > nums[mid] && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
